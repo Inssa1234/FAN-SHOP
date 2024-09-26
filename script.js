18109b7439f235331d5d55f7dc8c90dfdaec604d
Inssa1234 <142233429+Inssa1234@users.noreply.github.com>
2024-09-26 22:03:53 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const phrases = [
+  "des produits de qualité.",
+  "un service exceptionnel.",
+  "des packs incroyables.",
+  "Chez FAN-SHOP",
+];
+let currentPhrase = 0;
+
+function changeText() {
+  const textElement = document.getElementById("text");
+
+  textElement.classList.add("hidden");
+
+  setTimeout(() => {
+    currentPhrase = (currentPhrase + 1) % phrases.length;
+    textElement.textContent = phrases[currentPhrase];
+    textElement.classList.remove("hidden");
+  }, 500);
+}
+
+setInterval(changeText, 4000);
+changeText();
+
+window.addEventListener("scroll", () => {
+  let dialog = document.getElementById("scrollDialog");
+
+  if (window.scrollY > 100) {
+    dialog.classList.add("show");
+  } else {
+    dialog.classList.remove("show");
+  }
+  let dialog2 = document.getElementById("scrollDialog2");
+
+  if (window.scrollY > 400) {
+    dialog2.classList.add("show2");
+  } else {
+    dialog2.classList.remove("show2");
+  }
+});
